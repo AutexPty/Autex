b251f8258555b629ac50528d67001b7ba807090e
Dean Connell <deanc@autex.com.au>
2017-02-10 16:04:21 +1100

    Update addproduct.js

--- js/addproduct.js
+++ js/addproduct.js
@@ -7,5 +7,26 @@ function addproduct() {
 function selectcategory() {
  document.getElementById("dproductcat").hidden = true;
  document.getElementById("dproduct").hidden = false; 
+  
+ selectedcategory = $("#category").value
+
+  if (selectedcategory) {
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    } else {    // IE 5/6
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.open("GET", "./" & selectedcategory & ".xml", false);
+    xhttp.send(null);
+    xhttp.onreadystatechange = function(){
+    if (xhttp.status == "200")
+      xmlDoc = xhttp.responseXML; 
+    }
+ debugger;  
+ var uurloon = xmlDoc.getElementsByTagName("uurloon")[0].childNodes[0].textContent;
+ var setloon = xmlDoc.getElementsByTagName("setloon")[0].childNodes[0].textContent;  
+  
+  }
  debugger;
-}
+  
+  }
