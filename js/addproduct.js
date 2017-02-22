bfcec2d8af7de64186a18015eafe66aebec8f760
Dean Connell <deanc@autex.com.au>
2017-02-22 17:13:36 +1100

    Update addproduct.js

--- js/addproduct.js
+++ js/addproduct.js
@@ -83,14 +83,23 @@ function todescription(){
 
 function addproduct() {
   var comment =  document.getElementById('description').value;
-  var pcode = products[item].getElementsByTagName("productcode")[0].textContent + selvar + colour
-  var productstr = "product?productcode="+pcode;
-  if (selvar) {productstr = productstr + "?variant=" + selvar;};
-  if (colour) {productstr = productstr + "?colour="  + colour;};
+  var pcode = products[item].getElementsByTagName("productcode")[0].textContent 
+  
+  + selvar + colour
+  var productstr = "";
+  
+  if (selvar) {
+               productstr = productstr + "?variant=" + selvar;
+               pcode = pcode +selvar
+              };
+  if (colour) {
+               productstr = productstr + "?colour="  + colour;
+               pcode = pcode + colour
+              };
   if (quantity) {productstr = productstr + "?Qty=" + quantity;};
   if (unit) {productstr = productstr + "?UOM=" + unit;};
   
-  t.attach({ url: productstr, name: comment })
+  t.attach({ url: "product?productcode=" + pcode + productstr, name: comment })
   t.closePopup();
 }
 
