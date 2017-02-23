30767aa5bab76e475c8577b949c61b01be4996d4
Dean Connell <deanc@autex.com.au>
2017-02-23 11:04:52 +1100

    Update section.js

--- js/section.js
+++ js/section.js
@@ -1,6 +1,7 @@
 /* global TrelloPowerUp */
 
 var t = TrelloPowerUp.iframe();
+var tb = document.getElementById('producttable').createTBody();
 
 // you can access arguments passed to your iframe like so
 var arg = t.arg('arg');
@@ -15,9 +16,21 @@ t.render(function(){
     return attachment.url.indexOf('http://product?') == 0;
   })
   .then(function(yellowstoneAttachments){
-    var urls = yellowstoneAttachments.map(function(a){ return a.url; });
-    document.getElementById('urls').textContent = urls.join(', ');
-    debugger;
+    var urls = yellowstoneAttachments.map(
+      function(a){ 
+        ir = tb.insertRow();
+        c = ir.insertCell();
+
+        ir = tb.insertRow();
+        c = ir.insertCell();
+        c.textContent=a.name;
+
+        ir = tb.insertRow();
+        c = ir.insertCell();        
+        //return a.url; 
+      });
+    //document.getElementById('urls').textContent = urls.join(', ');
+    //debugger;
   })
   .then(function(){
     return t.sizeTo('#content');
