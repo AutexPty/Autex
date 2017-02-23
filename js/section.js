5924076438334dedd1b4b723a1b9e5e0f7862529
Dean Connell <deanc@autex.com.au>
2017-02-23 11:38:52 +1100

    Update section.js

--- js/section.js
+++ js/section.js
@@ -16,13 +16,29 @@ t.render(function(){
     return attachment.url.indexOf('http://product?') == 0;
   })
   .then(function(yellowstoneAttachments){
+    var pcode = "";
+    var qty = "";
+    var uom = "";
+    
     var urls = yellowstoneAttachments.map(
       function(a){ 
+        b = a.url.split('?'); //divide the url into portions
+        for (i = 0; i < b.length; i++) {
+          if (b[i].indexOf("productcode=")) {;pcode = b[i].slice(12)};
+          if (b[i].indexOf("Qty=")) {; = b[i].slice(4)};
+          if (b[i].indexOf("UOM=")) {;uom = b[i].slice(4)};                   
+        }
+        
         ir = tb.insertRow();
         c = ir.insertCell();
+        c.textContent=pcode;
         c = ir.insertCell();
         c.textContent=a.name;
         c = ir.insertCell();        
+        c.textContent=qty;
+        c = ir.insertCell();        
+        c.textContent=uom;
+
         //return a.url; 
       });
     //document.getElementById('urls').textContent = urls.join(', ');
