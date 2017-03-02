ee6fe1df439e333ee022f673dae461f81165eafb
Dean Connell <deanc@autex.com.au>
2017-03-02 16:38:06 +1100

    Update section.js

--- js/section.js
+++ js/section.js
@@ -20,6 +20,10 @@ t.render(function(){
     var pcode = "";
     var qty = "";
     var uom = "";
+    var m2 = "";
+    var w = "";
+    var l = "";
+    var c = "";
     
     var urls = yellowstoneAttachments.map(
       function(a){ 
@@ -27,7 +31,11 @@ t.render(function(){
         for (i = 0; i < b.length; i++) {
           if (b[i].indexOf("productcode=")==0) {pcode = b[i].slice(12);};
           if (b[i].indexOf("Qty=")==0) {qty = b[i].slice(4);};
-          if (b[i].indexOf("UOM=")==0) {uom = b[i].slice(4);};                   
+          if (b[i].indexOf("UOM=")==0) {uom = b[i].slice(4);};   
+          if (b[i].indexOf("m2=")==0)  {m2 =  b[i].slice(3);};   
+          if (b[i].indexOf("w=")==0)   {w =   b[i].slice(2);};   
+          if (b[i].indexOf("l=")==0)   {l =   b[i].slice(2);};             
+          if (b[i].indexOf("comments=")==0)   {c =   b[i].slice(9);};             
         }
         
         ir = tb.insertRow();
@@ -39,6 +47,14 @@ t.render(function(){
         c.textContent=qty;
         c = ir.insertCell();        
         c.textContent=uom;
+        c = ir.insertCell();        
+        c.textContent=m2;
+        c = ir.insertCell();        
+        c.textContent=w;
+        c = ir.insertCell();        
+        c.textContent=l;        
+        c = ir.insertCell();        
+        c.textContent=c;       
       });
   })
   .then(function(){
