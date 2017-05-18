978c3e1b3862d61903e2977295cc05a617df6d02
Dean Connell <deanc@autex.com.au>
2017-05-18 18:16:27 +1000

    Update adddocument.js

--- js/adddocument.js
+++ js/adddocument.js
@@ -4,6 +4,6 @@ var t = TrelloPowerUp.iframe();
 function save() {
   var sp = document.getElementById('StatePrefix').value
   var dn = document.getElementById('DocumentNo').value
-  t.attach({ url: "http://document?" + sp + dn, name: "Document " + sp + dn})
+  t.attach({ url: "http://document?" + sp + dn, name: "#" + sp + dn})
   t.closePopup();
 }
