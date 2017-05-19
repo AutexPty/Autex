badae8136c4480c7b74f4b57c4c20666cd08cabc
Dean Connell <deanc@autex.com.au>
2017-05-19 12:09:18 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -7,14 +7,16 @@ function tabledata() {
      //alasql("CREATE TABLE data (due DATETIME, url STRING,name STRING)");
      for (i=0; i<promiseResult.length;i++) {    
          if (promiseResult[i].attachments.length > 0) {
-            //for (ii=0; ii<promiseResult[i].attachments.length;ii++) {      
+            for (ii=0; ii<promiseResult[i].attachments.length;ii++) {      
             //    var sql="INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");"
             //    alasql(sql);
-            // };   
-              console.log(i);
-            };
+              console.log(i&"-"&ii);                          
+             };   //end of attachment loop
+            }; // end if attachment length > 0 
+              console.log(i);       
+     }; //end loop of cards.
          console.log('ok');
-     };
+    
     //console.log( alasql("SELECT * FROM data ORDER BY due") );
     
     //alert('hi');
