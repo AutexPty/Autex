e151919fa0259d9e1099c527722a404fcd5a7e2b
Dean Connell <deanc@autex.com.au>
2017-05-19 11:51:47 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -1,16 +1,17 @@
 var t = TrelloPowerUp.iframe();
 
 function tabledata() {
-t.cards('id', 'name', 'url','due','attachments')
-.then(function(promiseResult) {
-  alasql("CREATE TABLE data (due DATETIME, url STRING,name STRING)");
-  for (i=0; i<promiseResult.length;i++) {    
-    if (promiseResult[i].attachments.length > 0) {
-      for (ii=0; ii<promiseResult[i].attachments.length;ii++) {      
-        alasql("INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");");
-      }
-    }    
-  }
+  t.cards('id', 'name', 'url','due','attachments')
+   .then(function(promiseResult) {
+     alasql("CREATE TABLE data (due DATETIME, url STRING,name STRING)");
+     for (i=0; i<promiseResult.length;i++) {    
+         if (promiseResult[i].attachments.length > 0) {
+            for (ii=0; ii<promiseResult[i].attachments.length;ii++) {      
+                var sql="INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");"
+                alasql(sql);
+            };
+         };    
+     };
     console.log( alasql("SELECT * FROM data ORDER BY due") );
 });
-}
+};
