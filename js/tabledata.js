cd9e3d101989fc5ca8d874b5bcd69594f98643cd
Dean Connell <deanc@autex.com.au>
2017-05-19 11:03:49 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -2,5 +2,9 @@ var t = TrelloPowerUp.iframe();
 
 function tabledata() {
   alert('hi');
+  t.cards('id', 'name', 'desc', 'due').get('name').then(function(name){
+  var n = name;
+    
+  });
   t.closePopup();
 }
