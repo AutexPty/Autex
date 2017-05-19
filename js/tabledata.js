6f6d95ecb376178867280cce247cd8715e8ab434
Dean Connell <deanc@autex.com.au>
2017-05-19 12:53:14 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -1,11 +1,16 @@
 var t = TrelloPowerUp.iframe();
 
 function processrecord(data) {
+    var output = {}
     var v = data.filter(function(text){ return text.indexOf('variant=') == 0 })[0];
     var p = data.filter(function(text){ return text.indexOf('productcode=') == 0 })[0];
     var q = data.filter(function(text){ return text.indexOf('Qty=') == 0 })[0];
     var c = data.filter(function(text){ return text.indexOf('colour=') == 0 })[0];  
-    return { variant: v.split('=')[1] , productcode: p.split('=')[1] ,qty: q.split('=')[1],colour: c.split('=')[1] } 
+        if (v) {output.variant = v.split('=')[1];};
+    if (p) {output.productcode = p.split('=')[1];};
+    if (q) {output.qty = q.split('=')[1];};
+    if (c) {output.colour = c.split('=')[1];};
+    return output; 
 };
 
 function tabledata() {
