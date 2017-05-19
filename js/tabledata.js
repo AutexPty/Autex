c72c8502497bace7878ab7960ec4de48c7675ed0
Dean Connell <deanc@autex.com.au>
2017-05-19 12:33:54 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -1,5 +1,13 @@
 var t = TrelloPowerUp.iframe();
 
+function processrecord(data) {
+    var v = data.filter(function(text){ return text.indexOf('variant=') == 0 });
+    var p = data.filter(function(text){ return text.indexOf('productcode=') == 0 });
+    var q = data.filter(function(text){ return text.indexOf('qty=') == 0 });
+    var c = data.filter(function(text){ return text.indexOf('colour=') == 0 });  
+    return { variant: v.split('=')[1] , productcode: p.split('=')[1] ,qty: q.split('=')[1],colour: c.split('=')[1] } 
+};
+
 function tabledata() {
   t.cards('id', 'name', 'url','due','attachments')
    .then(function(promiseResult) {
@@ -7,10 +15,12 @@ function tabledata() {
      //alasql("CREATE TABLE data (due DATETIME, url STRING,name STRING)");
      for (i=0; i<promiseResult.length;i++) {    
          if (promiseResult[i].attachments.length > 0) {
-            for (ii=0; ii<promiseResult[i].attachments.length;ii++) {      
-                var sql="INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");"
+            for (ii=0; ii<promiseResult[i].attachments.length;ii++) {
+              var data = promiseResult[i].attachments[ii].url.split('?');
+              var pdata = processrecord(data);
+                //var sql="INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");"
             //    alasql(sql);
-                console.log(sql);                          
+                console.log(pdata);                          
              };   //end of attachment loop
             }; // end if attachment length > 0 
               console.log(i);       
