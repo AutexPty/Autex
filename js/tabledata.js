64fc25955022754cb02fa51b4df67949f513f8ba
Dean Connell <deanc@autex.com.au>
2017-05-19 11:27:58 +1000

    Update tabledata.js

--- js/tabledata.js
+++ js/tabledata.js
@@ -1,8 +1,13 @@
 var t = TrelloPowerUp.iframe();
 
 function tabledata() {
-t.cards('id', 'name', 'url')
+t.cards('id', 'name', 'url','due')
 .then(function(promiseResult) {
   console.log(promiseResult);
+    alasql("CREATE TABLE test (language INT, hello STRING)");
+    alasql("INSERT INTO test VALUES (1,'Hello!')");
+    alasql("INSERT INTO test VALUES (2,'Aloha!')");
+    alasql("INSERT INTO test VALUES (3,'Bonjour!')");
+    console.log( alasql("SELECT * FROM test WHERE language > 1") );
 });
 }
