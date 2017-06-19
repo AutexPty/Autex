4be8437c180d4955b60ad4e315493decb115048d
Dean Connell <deanc@autex.com.au>
2017-06-19 14:30:17 +1000

    use lists instead of cards.

--- reports/js/region.js
+++ reports/js/region.js
@@ -2,32 +2,37 @@ var t = TrelloPowerUp.iframe();
 
 function getregiondata() {
   tbl = document.getElementById('table');
-  t.cards('id', 'name', 'url','due','attachments')
+  t.lists('all')
    .then(function(promiseResult) {
     
      //alasql("CREATE TABLE data (due DATETIME, url STRING,name STRING)");
-     for (i=0; i<promiseResult.length;i++) {    
-         if (promiseResult[i].attachments.length > 0) {
-            for (ii=0; ii<promiseResult[i].attachments.length;ii++) {
-                if (promiseResult[i].attachments[ii].url.indexOf('http://region') == 0 ) 
-                {
-                   list = document.getElementById("list");
-                   if (list.item(list.selectedIndex).text == promiseResult[i].attachments[ii].name) 
-                   {
-                     var row = tbl.insertRow(0);
-                     var cardname = row.insertCell(0);
-                     var carddue = row.insertCell(0); 
-                     var cardregion = row.insertCell(0); 
-                     cardname.innerHTML = "<a href=" + promiseResult[i]["url"] + ">"+ promiseResult[i]["name"]+"</a>";
-                     carddue.innerHTML = promiseResult[i]["due"];
-                     cardregion.innerHTML = promiseResult[i].attachments[ii].name;
-                   };                                                             
-                };
-              };   //end of attachment loop
-            }; // end if attachment length > 0 
-              console.log(i);       
+     for (l=0; l<promiseResult.length;l++) {    
+         var currentlist = promiseResult[l];
+         for (c=0; c<promiseResult[l].cards.length;c++) 
+           {
+           var currentcard = currentlist.cards[c];           
+           if (currentcard.attachments.length > 0) 
+              {
+              for (ii=0; ii<currentcard.attachments.length;ii++) 
+                 {
+                   var currentattachment = currentcard.attachments[ii];
+                   if (currentattachment.url.indexOf('http://region') == 0 ) 
+                     {
+                     if (list.item(list.selectedIndex).text == currentattachment.name) 
+                        {
+                        var row = tbl.insertRow(0);
+                        var cardname = row.insertCell(0);
+                        var carddue = row.insertCell(0); 
+                        var cardregion = row.insertCell(0); 
+                        cardname.innerHTML = "<a href=" + promiseResult[i]["url"] + ">"+ promiseResult[i]["name"]+"</a>";
+                        carddue.innerHTML = promiseResult[i]["due"];
+                        cardregion.innerHTML = promiseResult[i].attachments[ii].name;
+                        };                                                             
+                     };
+                 };   //end of attachment loop
+              }; // end if attachment length > 0 
+           }; //loop cards.
      }; //end loop of cards.
-         console.log('ok');
      tbl.hidden = false;    
 });
 };
