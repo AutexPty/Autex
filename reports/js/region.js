f93f8990ad1235a4dba5f551a0b0cf92190306de
Dean Connell <deanc@autex.com.au>
2017-06-19 13:35:18 +1000

    Update region.js

--- reports/js/region.js
+++ reports/js/region.js
@@ -1,29 +1,7 @@
 var t = TrelloPowerUp.iframe();
 
-function processrecord(data) {
-    var output = {}
-    var v = data.filter(function(text){ return text.indexOf('variant=') == 0 })[0];
-    var p = data.filter(function(text){ return text.indexOf('productcode=') == 0 })[0];
-    var q = data.filter(function(text){ return text.indexOf('Qty=') == 0 })[0];
-    var c = data.filter(function(text){ return text.indexOf('colour=') == 0 })[0];  
-        if (v) {output.variant = v.split('=')[1];};
-    if (p) {output.productcode = p.split('=')[1];};
-    if (q) {output.qty = q.split('=')[1];};
-    if (c) {output.colour = c.split('=')[1];};
-    return output; 
-};
-
-function listdata() {
-  t.lists('all')
-   .then(function(promiseResult) {
-      console.log(promiseResult);
-  });
-
-}
-
 function getregiondata() {
   tbl = document.getElementById('table');
-    
   t.cards('id', 'name', 'url','due','attachments')
    .then(function(promiseResult) {
     
@@ -47,25 +25,13 @@ function getregiondata() {
                      cardurl.innerHTML = promiseResult[i]["url"];
                      carddue.innerHTML = promiseResult[i]["due"];
                      cardcl.innerHTML = promiseResult[i].attachments[ii].name;
-                   };                    
-                   //var data = promiseResult[i].attachments[ii].url.split('?');
-                   //var pdata = processrecord(data);      
-                   //console.log('i');
-                   //cardname.innerHTML = promiseResult[i].name;
-                   //qty.innerHTML = pdata.qty;
-                   //des.innerHTML = pdata.variant + " " + pdata.colour;                                           
+                   };                                                             
                 };
-                
-                //var sql="INSERT INTO data VALUES (" & promiseResult[i].due & "," & promiseResult[i].attachments[ii].url & "," & promiseResult[i].name & ");"
-            //    alasql(sql);
-             };   //end of attachment loop
+              };   //end of attachment loop
             }; // end if attachment length > 0 
               console.log(i);       
      }; //end loop of cards.
          console.log('ok');
      tbl.hidden = false;    
-    //console.log( alasql("SELECT * FROM data ORDER BY due") );
-    
-    //alert('hi');
 });
 };
