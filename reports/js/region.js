fd2fec381ea6627a321bec50da496d9aac8c69a7
Dean Connell <deanc@autex.com.au>
2017-06-23 09:48:07 +1000

    Update region.js

--- reports/js/region.js
+++ reports/js/region.js
@@ -27,9 +27,10 @@ function getregiondata() {
                         {
                         var row = tbl.tBodies[0].insertRow(0);
                         // inserted cells are backwards (last first) because of reasons..
+                        var cardproducts = row.insertCell(0);
                         var carddue = row.insertCell(0); 
                         var cardname = row.insertCell(0);
-                        var cardstage = row.insertCell(0); 
+                        var cardstage = row.insertCell(0);
 
                         //cardname.innerHTML = '<a href="#" onlick="loadid(' + "'"+ currentcard["id"] +"'"+');">'+ currentcard["name"]+'</a>';
                         var ank = document.createElement('a');
@@ -39,6 +40,44 @@ function getregiondata() {
                         cardname.appendChild(ank);                          
                         carddue.innerHTML = currentcard["due"];
                         cardstage.innerHTML = currentlist.name;
+                        
+                        for (cca=0; cca < currentcard.attachments.length;cca++)
+                        {
+                          var ccao = currentcard.attachments[cca]
+                          if (ccao.url.indexOf('http://product?') == 0) {
+                            var ul = document.createElement('ul');
+                            cardproducts.appendChild(ul);
+                            var li = document.createElement('li');
+                            li.appendChild(document.createTextNode(ccau.name));
+                            ul.appendChild(li);
+                            
+                            var qty = 0;
+                            var uom = "";
+                            var m2 = 0;
+                            var w = 0;
+                            var l = 0; 
+                            var cmnt = "";
+                            varpcode = "";
+                            var u = 0;
+                            
+                            b = ccao.url.split('?'); //divide the url into portions
+                            for (i = 0; i < b.length; i++) {
+                              if (b[i].indexOf("productcode=")==0) {pcode = b[i].slice(12);};
+                              if (b[i].indexOf("Qty=")==0)         {qty = b[i].slice(4);};
+                              if (b[i].indexOf("UOM=")==0)         {uom = b[i].slice(4);};   
+                              if (b[i].indexOf("PPK=")==0)         {u  =  b[i].slice(4);};   
+                              if (b[i].indexOf("m2=")==0)          {m2 =  b[i].slice(3);};   
+                              if (b[i].indexOf("w=")==0)           {w =   b[i].slice(2);};   
+                              if (b[i].indexOf("l=")==0)           {l =   b[i].slice(2);};             
+                              if (b[i].indexOf("comments=")==0)    {cmnt =   b[i].slice(9);};             
+                            };
+                            
+                            
+                            
+                            
+                          }
+                        }
+                        
                         };                                                             
                      };
                  };   //end of attachment loop
