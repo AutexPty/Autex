a2093505a61f7f5d305f5b917b0c9f78a79da4e6
Dean Connell <deanc@autex.com.au>
2017-09-19 16:00:19 +1000

    Update products.js

--- reports/js/products.js
+++ reports/js/products.js
@@ -1,5 +1,18 @@
 var t = TrelloPowerUp.iframe();
+
+function removeOptions(selectbox)
+{
+    var i;
+    for(i = selectbox.options.length - 1 ; i >= 0 ; i--)
+    {
+        selectbox.remove(i);
+    }
+}
+//using the function:
+removeOptions(document.getElementById("mySelectObject"));
+
 function showlist(){
+  removeOptions(list);
   t.lists('all')
    .then(function(promiseResult) {
    for (l=0; l<promiseResult.length;l++) {
@@ -10,6 +23,6 @@ function showlist(){
      list.appendChild(opt);
    }   
   });        
-  alert('done');
+  //alert('done');
 };
 
