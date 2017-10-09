c6c2e0eb2d3f6eb02794e7aa17b3e45b8fb73f94
Dean Connell <deanc@autex.com.au>
2017-10-09 16:40:24 +1100

    Update products.js

--- reports/js/products.js
+++ reports/js/products.js
@@ -126,9 +126,9 @@ function showlist(){
                                      addline(sublist,"Meters Squared:",m2);
                                      if (pcode) {
                                         if (products[pcode])
-                                           {products[pcode].m2 += parseFloat(m2);}
+                                           {products[pcode].m2 += parseFloat(m2)}
                                         else 
-                                           {products[pcode] = {parseFloat(m2);}
+                                           {products[pcode] = {parseFloat(m2)}
                                         };
                                     };
                             if (w&&lx) {addline(sublist,"Dimensions:",w+' x '+lx);};
