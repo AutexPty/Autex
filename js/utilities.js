623e2594ba27e7309f1caa7553c4ebd0e4c87c57
Dean Connell <deanc@autex.com.au>
2017-12-11 10:30:20 +1100

    Update utilities.js

--- js/utilities.js
+++ js/utilities.js
@@ -7,10 +7,10 @@ function showlist(){
         var currentlist = promiseResult[0]
         console.log('LIST:' +currentlist["name"]);
          for (c=0; c<currentlist.cards.length;c++) 
-           {
+         {
            var currentcard = currentlist.cards[c];           
            if (currentcard.attachments.length > 0) 
-              {
+            {
                   console.log('CARD:('+currentcard["id"]+') ' + currentcard["name"]);                 
                     for (ii=0; ii<currentcard.attachments.length;ii++) 
                         {
@@ -18,14 +18,14 @@ function showlist(){
                             var ccao = currentcard.attachments[ii];
                             if (ccao.url.indexOf('http://product') == 0) {
                                 console.log(ccao.name);
-                                ccao.url.indexOf('http://product?') == 0)
-                                console.log('http://product.io/?' + ccao.url.substring(15, 999)
+                                if (ccao.url.indexOf('http://product?') == 0) {
+                                console.log('http://product.io/?' + ccao.url.substring(15, 999))
                                 };
-                        };
-              }
-           }
-      };
-    });
+								};
+						}
+			}	
+		 };
+      });
 }
 
 
