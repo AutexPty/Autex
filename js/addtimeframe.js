1a025dc06a94a23d2547950493f49c732960abb8
nehapatil7879 <nehap@autex.com.au>
2017-12-19 14:43:34 +1100

    Update addtimeframe.js

--- js/addtimeframe.js
+++ js/addtimeframe.js
@@ -2,6 +2,6 @@ var t = TrelloPowerUp.iframe();
 
 function save() {
   var sp = document.getElementById('timeframe').value
-  t.attach({ url: "http://timeframe.io/?" + sp, name: "#" + sp })
+  t.attach({ url: "http://timeframe.io/?" + sp, name: "Estimated project timeframe: " + sp })
   t.closePopup();
 }
