4a3a46add5780f46c1945736918a60810e671296
nehapatil7879 <nehap@autex.com.au>
2017-12-20 14:17:11 +1100

    Update addclassification.js

--- js/addclassification.js
+++ js/addclassification.js
@@ -3,6 +3,6 @@ var t = TrelloPowerUp.iframe();
 function save() {
   var sp = document.getElementById('StatePrefix').value
   var firstletter= sp.substring(0,1)
-  t.attach({ url: "http://classification.io/?" + firstletter, name: "Estimated Project Cost: " + sp.substring(2,sp.length-1) })
+  t.attach({ url: "http://classification.io/?" + firstletter, name: "Estimated Project Cost: " + sp.substring(2,sp.length) })
   t.closePopup();
 }
